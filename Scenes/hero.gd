6fa54fc5745fe2dce69a0631318ef117b412064d
eyedoteye <>
2022-11-14 14:12:16 -0500

    Add Jump n Run

--- Scenes/hero.gd
+++ Scenes/hero.gd
@@ -7,25 +7,96 @@ extends KinematicBody2D
 
 onready var _sprite = $Sprite
 onready var _animationTree = $AnimationTree
-onready var state_machine = _animationTree["parameters/playback"]
+onready var _stateMachine = _animationTree["parameters/playback"]
 
+
+var velocity = Vector2()
+var gravity = 200
+var jumpStrength = 200
+var moveSpeed = 90
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func processJumpInput() -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		_stateMachine.travel("Jump")
+		velocity.y -= jumpStrength
+
+func processFalling() -> void:
+	if velocity.y > 0:
+		_stateMachine.travel("Jump")
+
+func applyMovement(delta) -> void:
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func processDirMovement(dir) -> void:
+	if dir > 0:
+		velocity.x = moveSpeed
+	elif dir < 0:
+		velocity.x = -moveSpeed
+	else:
+		velocity.x = 0
+
+func processInAirDirMovement(dir) -> void:
+	if _sprite.flip_h:
+		if dir > 0:
+			velocity.x = moveSpeed
+		elif dir < 0:
+			velocity.x = -moveSpeed / 2.2
+	else:
+		if dir > 0:
+			velocity.x = moveSpeed / 2.2
+		elif dir < 0:
+			velocity.x = -moveSpeed
+
+func flipSpriteToDir(dir) -> void:
+	if dir == 1:
+		_sprite.flip_h = true
+	elif dir == -1:
+		_sprite.flip_h = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-#	state_idle.visible = false
-#	state_run.visible = true
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity)
 	
-	if Input.is_action_pressed("ui_left"):		
-		_sprite.flip_h = false
-		state_machine.travel("Run")
-	elif Input.is_action_pressed("ui_right"):
-		_sprite.flip_h = true
-		state_machine.travel("Run")
-	elif Input.is_action_pressed("ui_accept"):
-		state_machine.travel("Jump")
-	else:
-		state_machine.travel("Idle")
+	var dir = 0
+	if Input.is_action_pressed("ui_left"):
+		dir -= 1
+	if Input.is_action_pressed("ui_right"):
+		dir += 1
+	
+	
+	var currentState =  _stateMachine.get_current_node()
+	match currentState:
+		"Jump":
+			processInAirDirMovement(dir)
+			
+			applyMovement(delta)
+			if is_on_floor() or velocity.y == 0:
+				if dir == 0:
+					_stateMachine.travel("Idle")
+				else:
+					_stateMachine.travel("Run")
+		"Idle":
+			processJumpInput()
+			processFalling()
+			processDirMovement(dir)
+			
+			applyMovement(delta)
+			
+			flipSpriteToDir(dir)
+			if dir != 0:
+				_stateMachine.travel("Run")
+		"Run":
+			processJumpInput()
+			processFalling()
+			processDirMovement(dir)
+			
+			applyMovement(delta)
+			
+			flipSpriteToDir(dir)
+			if dir == 0:
+				_stateMachine.travel("Idle")
